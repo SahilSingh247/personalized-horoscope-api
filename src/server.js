23f25a353bfd0af127399eeb7e7e3703bfbaf346
SahilSingh247 <sahilsinghrathore247@gmail.com>
2025-11-01 11:52:10 +0530

    rate limit using the express-rate-limit module

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 const authRoutes = require('./routes/jwtAuth');
+const rateLimit = require('./middleware/ratelimit');
 const horoscopeRoutes = require('./routes/horoscopeRoutes');
 const authenticateToken = require('./middleware/authToken');
 app.use(express.json());
 
-app.use('/api', authRoutes);
+app.use('/api', rateLimit, authRoutes);
 app.use('/api/horoscope', authenticateToken, horoscopeRoutes);
 
 const PORT = 3000;
